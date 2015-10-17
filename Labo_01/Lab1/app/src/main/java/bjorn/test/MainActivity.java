9ae6ff02f29fb22b8fca942cc3a00d429eca3d70
vdbbjorn <bjorn.vandenbussche@ugent.be>
2015-10-17 09:24:09 +0200

    lab1-2-3

---
+++ Labo_01/Lab1/app/src/main/java/bjorn/test/MainActivity.java
@@ -0,0 +1,54 @@
+package bjorn.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button b = (Button) findViewById(R.id.button_click);
+        b.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void clickFunction(View view) {
+        Toast.makeText(MainActivity.this,"Button clicked!", Toast.LENGTH_LONG).show();
+        TextView v = (TextView) findViewById(R.id.textBlock);
+        v.append(" (clicked)");
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
