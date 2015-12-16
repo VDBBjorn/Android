5998e55f5fa55049e162d4c4fe8909c6e39f9183
Bjorn Vandenbussche <vandenbusschebjorn@gmail.com>
2015-12-16 13:19:24 +0100

    labo 9 done

---
+++ Labo_09/Bootstrap/public_html/todo/www/js/cordova.js
@@ -0,0 +1,6 @@
+cordova.plugins.notification.local.schedule({
+    id: 1,
+    text: "Single Notification",
+    sound: isAndroid ? 'file://sound.mp3' : 'file://beep.caf',
+    data: { secret:key }
+});
